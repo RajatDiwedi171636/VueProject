acc9986460a61706284a9b631bdb15dfeb5e77f1
Rajat Diwedi <rajat.diwedi@jadeglobal.com>
2023-02-02 14:41:31 +0530

    first commit of Vue Project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <app-header></app-header>
+     <app-ninja></app-ninja>
+     <app-footer></app-footer>
+     
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import header from './components/component_header.vue'
+import footer from './components/component_footer.vue'
+import ninja from './components/component_ninja.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    "app-header":header,
+    "app-footer":footer,
+    "app-ninja":ninja
+
+
+  },
+  data(){
+    return {
+         
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
